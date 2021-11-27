e79929165627006ac26b8724463cd9957ab4d9e4
YeinsM <wildermancera1@gmail.com>
2021-11-27 15:24:51 -0400

    Reto1y2 1er INtento

---
+++ Persistencia/Migrations/20211127171914_addId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistencia.Migrations
+{
+    public partial class addId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Pais",
+                table: "Migrantes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Migrantes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "userName",
+                table: "Migrantes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Migrantes");
+
+            migrationBuilder.DropColumn(
+                name: "userName",
+                table: "Migrantes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pais",
+                table: "Migrantes",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
